2d8474607163f678805afffa11e622e36f7bca2d
Thalys Penha <thalyspenha@outlook.com.br>
2026-07-28 14:09:06 -0300

    feat: tema Gruvbox para o SDDM (sddm-astronaut-theme)

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -1,2 +1,52 @@
-# modules/sddm-theme.nix (placeholder temporário, substituído na Task 4)
-{ }
+{ pkgs, ... }:
+let
+  palette = import ../gruvbox-palette.nix;
+
+  theme = pkgs.sddm-astronaut.override {
+    themeConfig = {
+      HeaderTextColor = "#${palette.fg1}";
+      DateTextColor = "#${palette.fg1}";
+      TimeTextColor = "#${palette.orange}";
+
+      FormBackgroundColor = "#${palette.bg0}";
+      BackgroundColor = "#${palette.bg0}";
+      DimBackgroundColor = "#${palette.bg0}";
+
+      LoginFieldBackgroundColor = "#${palette.bg1}";
+      PasswordFieldBackgroundColor = "#${palette.bg1}";
+      LoginFieldTextColor = "#${palette.fg1}";
+      PasswordFieldTextColor = "#${palette.fg1}";
+      UserIconColor = "#${palette.fg1}";
+      PasswordIconColor = "#${palette.fg1}";
+
+      PlaceholderTextColor = "#${palette.gray}";
+      WarningColor = "#${palette.red}";
+
+      LoginButtonTextColor = "#${palette.bg0}";
+      LoginButtonBackgroundColor = "#${palette.orange}";
+      SystemButtonsIconsColor = "#${palette.fg1}";
+      SessionButtonTextColor = "#${palette.fg1}";
+      VirtualKeyboardButtonTextColor = "#${palette.fg1}";
+
+      DropdownTextColor = "#${palette.fg1}";
+      DropdownSelectedBackgroundColor = "#${palette.bg2}";
+      DropdownBackgroundColor = "#${palette.bg0}";
+
+      HighlightTextColor = "#${palette.bg0}";
+      HighlightBackgroundColor = "#${palette.orange}";
+      HighlightBorderColor = "#${palette.orange}";
+
+      HoverUserIconColor = "#${palette.yellow}";
+      HoverPasswordIconColor = "#${palette.yellow}";
+      HoverSystemButtonsIconsColor = "#${palette.yellow}";
+      HoverSessionButtonTextColor = "#${palette.yellow}";
+      HoverVirtualKeyboardButtonTextColor = "#${palette.yellow}";
+    };
+  };
+in
+{
+  services.displayManager.sddm = {
+    theme = "sddm-astronaut-theme";
+    extraPackages = [ theme ];
+  };
+}
